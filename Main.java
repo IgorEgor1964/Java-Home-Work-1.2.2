958984fd7f264ceb1c6c94fc91976bab01ffed2a
igoregor1964 <77262709+IgorEgor1964@users.noreply.github.com>
2021-03-05 20:16:16 +1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int price = 150;
-    int count = 5;
-    int total = price * count;
-    System.out.println(total);
+    double regularBonus = 0.3;
+    double specialBonus = 0.6;
+    double totalBonus = regularBonus + specialBonus;
+    System.out.println(totalBonus);
   }
 }
-
